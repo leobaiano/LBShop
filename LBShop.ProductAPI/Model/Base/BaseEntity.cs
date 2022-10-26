7a8a5238a2327186660aa8d9eee25f25dbce9756
Leo Baiano <ljunior2005@gmail.com>
2022-10-26 12:05:41 -0300

    feat: cria propriedades de produto

--- LBShop.ProductAPI/Model/Base/BaseEntity.cs
+++ LBShop.ProductAPI/Model/Base/BaseEntity.cs
@@ -5,27 +5,8 @@ namespace LBShop.ProductAPI.Model.Base
 {
     public class BaseEntity
     {
-        [Column("name")]
-        [Required]
-        [StringLength(150)]
-        public string? Name { get; set; }
-
-        [Column("price")]
-        [Required]
-        [Range(1,10000)]
-        public string? Price { get; set; }
-
-        [Column("description")]
-        [StringLength(500)]
-        public string? Description { get; set; }
-
-        [Column("category_name")]
-        [StringLength(50)]
-        public string? CategoryName { get; set; }
-
-        [Column("image_url")]
-        [StringLength(300)]
-        public string? ImageURL { get; set; }
-
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
     }
 }
