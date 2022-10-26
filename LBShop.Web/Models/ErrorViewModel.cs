5d47db953b060e8414ba64816565c083acc0b721
Leo Baiano <ljunior2005@gmail.com>
2022-10-26 08:10:00 -0300

    feat: initial files service Web

---
+++ LBShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LBShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
