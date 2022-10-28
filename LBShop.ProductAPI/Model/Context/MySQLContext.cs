1d608c3ba24d890954399e40719c0b216fb1d816
Leo Baiano <ljunior2005@gmail.com>
2022-10-28 14:42:46 -0300

    feat: adiciona dependencias

--- LBShop.ProductAPI/Model/Context/MySQLContext.cs
+++ LBShop.ProductAPI/Model/Context/MySQLContext.cs
@@ -5,7 +5,7 @@ namespace LBShop.ProductAPI.Model.Context
     public class MySQLContext : DbContext
     {
         public MySQLContext() {}
-        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
 
