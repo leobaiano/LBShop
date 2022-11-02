5d7025bd2e03797f89fe0da6a6d7ebe584f958bb
Leo Baiano <ljunior2005@gmail.com>
2022-11-02 11:46:11 -0300

    feat: cria o repository de product

--- LBShop.ProductAPI/Data/ValueObjects/ProductVO.cs
+++ LBShop.ProductAPI/Data/ValueObjects/ProductVO.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace LBShop.ProductAPI.Data.ValueObjects
+﻿namespace LBShop.ProductAPI.Data.ValueObjects
 {
     public class ProductVO
     {
--- LBShop.ProductAPI/Repository/IProductRepository.cs
+++ LBShop.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace LBShop.ProductAPI.Repository
+﻿using LBShop.ProductAPI.Data.ValueObjects;
+
+namespace LBShop.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
     }
 }
