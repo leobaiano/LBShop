cc110fa6e1822993ec91d7b35b5b15fed74e31bd
Leo Baiano <ljunior2005@gmail.com>
2022-11-04 17:41:52 -0300

    feat: cria model product

---
+++ LBShop.Web/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LBShop.Web.Models
+{
+    public class ProductModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Price { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
+        public string ImageURL { get; set; } = string.Empty;
+    }
+}
